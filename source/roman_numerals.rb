85d36b3e1eb6bcdf0d1df241ca9a6647819bba36
Apprentice <apprentice@dbc05.local>
2014-01-06 20:24:42 -0800

    Release 1: Modern Roman Numerals

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,5 +1,25 @@
 def to_roman(num)
   # Your code here
+  last_digit = num.to_s.split().last.to_i
+  roman_numeral = []
+  numeral_values = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000=> "M"}
+
+  numeral_values.keys.reverse.each do |x|
+    letter_count = num / x
+    roman_numeral += Array.new(letter_count, numeral_values[x])
+    num = num % x
+  end
+
+  if last_digit == 9
+    roman_numeral.pop(5)
+    roman_numeral << "IX"
+  elsif last_digit == 4
+    roman_numeral.pop(4)
+    roman_numeral << "IV"
+  end
+
+
+  roman_numeral.join
 end
 
 # Drive code... this should print out trues.
@@ -7,5 +27,7 @@ end
 puts to_roman(1) == "I"
 puts to_roman(3) == "III"
 puts to_roman(6) == "VI"
+puts to_roman(1500) == "MD"
+puts to_roman(4) == "IV"
 
 # TODO: what other cases could you add to ensure your to_roman method is working?
