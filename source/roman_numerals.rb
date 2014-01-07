626d76d9f885ec8a1ca78217660e2b7a1d9aebef
Marion <knight499@gmail.com>
2014-01-06 21:08:17 -0800

    Original files added

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,5 +1,22 @@
 def to_roman(num)
-  # Your code here
+  roman_numeral = ""
+  arabic_numerals = {
+    "M" => 1000,
+    "D" => 500,
+    "C" => 100,
+    "L" => 50,
+    "X" => 10,
+    "V" => 5,
+    "I" => 1}
+  arabic_numerals.each do |k, v|
+  if num/v > 0
+    roman_numeral += (k*(num/v))
+    num = num%v
+   else
+    roman_numeral
+   end
+  end
+  roman_numeral
 end
 
 # Drive code... this should print out trues.
