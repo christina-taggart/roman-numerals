04a035f28745a8cb5ff6cce4d49a43aa260632a3
eli shkurkin <eshkurkin@gmail.com>
2014-01-06 23:42:59 -0800

    release 0

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,5 +1,13 @@
 def to_roman(num)
-  # Your code here
+  roman_vals = {"M" => 1000, "D" => 500,
+               "C" => 100, "L" => 50,
+               "X" => 10, "V" => 5, "I" => 1}
+  converted = ""
+  roman_vals.each do |key, val|
+    converted << key * (num / val)
+    num = num % val
+  end
+  converted
 end
 
 # Drive code... this should print out trues.
@@ -9,3 +17,8 @@ puts to_roman(3) == "III"
 puts to_roman(6) == "VI"
 
 # TODO: what other cases could you add to ensure your to_roman method is working?
+
+puts to_roman(10) == "X"
+puts to_roman(50) == "L"
+puts to_roman(100) == "C"
+puts to_roman(2654) == "MMDCLIIII"
