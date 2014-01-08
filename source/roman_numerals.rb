3f3a513674bc9b036429c10be263c5b20bd49c6b
Apprentice <apprentice@dbc31.local>
2014-01-08 12:27:07 -0800

    started roman numerals

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,11 +1,21 @@
 def to_roman(num)
-  # Your code here
+  roman_final = ""
+  romans = { 1 => "I"
+             }
+   romans.each do |arabic, roman|
+      # if num == arabic
+        roman_final = roman * num
+      # end
+   end
+   roman_final
 end
 
 # Drive code... this should print out trues.
 
 puts to_roman(1) == "I"
+puts to_roman(2) == "II"
 puts to_roman(3) == "III"
-puts to_roman(6) == "VI"
+puts to_roman(5) == "V"
+# puts to_roman(6) == "VI"
 
 # TODO: what other cases could you add to ensure your to_roman method is working?
