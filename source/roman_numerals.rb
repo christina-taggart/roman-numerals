53374520faea7cdd3e346be9092821ac71b8f3d7
Spencer Smitherman <null>
2014-01-11 19:35:06 -0800

    Completed release 0

--- source/roman_numerals.rb
+++ source/roman_numerals.rb
@@ -1,5 +1,25 @@
-def to_roman(num)
-  # Your code here
+# Passed in an Arabic numeral => return a Roman numeral.
+# Store all Roman numerals in a hash with the Arabic numerals as their pairs.
+# Given a specific Arabic numeral, iterate through the hash from
+# highest to lowest subtracting values as it iterates.
+# Append values to an array and convert it to a string for output.
+def to_roman(number)
+  solution = ""
+  numerals = { 1000 => "M",
+               500 => "D",
+               100 => "C",
+               50 => "L",
+               10 => "X",
+               5 => "V",
+               1 => "I" }
+
+  numerals.each do |arabic, roman|
+    while arabic <= number
+      solution << roman
+      number -= arabic
+    end
+  end
+  p solution
 end
 
 # Drive code... this should print out trues.
@@ -7,5 +27,7 @@ end
 puts to_roman(1) == "I"
 puts to_roman(3) == "III"
 puts to_roman(6) == "VI"
+puts to_roman(9) == "VIIII"
+puts to_roman(1646) == "MDCXXXXVI"
 
 # TODO: what other cases could you add to ensure your to_roman method is working?
